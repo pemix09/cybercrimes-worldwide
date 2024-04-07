8de47a41cfa898410dfbec7b439e607ee8b9de9a
pemix09 <pemix09@onet.pl>
2024-04-07 16:26:36 +0200

    Script saves output to file

--- localization_gathering.sh
+++ localization_gathering.sh
@@ -2,26 +2,24 @@
 
 # get filename from first argument
 filename="$1"
+destinationFileName="processed-$filename"
+processedLineNumber=0
 
 #from file read columns and save them to variables
 while IFS=, read -r Timestamp SourceIP DestinationIP SourcePort DestinationPort Protocol PacketLength PacketType TrafficType PayloadData MalwareIndicators AnomalyScores Alerts AttackType AttackSignature ActionTaken SeverityLevel UserInformation DeviceInformation NetworkSegment GeoLocation ProxyInformation FirewallLogs IDSIPSAlerts LogSource
 do
-    echo "----------------------------"
-
     # -i - ignore case -E - pattern -A - after match
     sourceInfo=$(whois $SourceIP | grep -i -E 'city:|country:')
     sourceCity=$( echo "$sourceInfo" | grep -i -E 'city:|City:' | cut -w -f2)
     sourceCountry=$( echo "$sourceInfo" | grep -i -E 'country:|Country:' | cut -w -f2)
-
-    echo "source city: $sourceCity"
-    echo "source country: $sourceCountry"
-    
     
     destInfo=$(whois $DestinationIP | grep -i -E 'city:|country:')
     destCity=$( echo "$destInfo" | grep -i -E 'city:|City:' | cut -w -f2)
     destCountry=$( echo "$destInfo" | grep -i -E 'country:|Country:' | cut -w -f2)
 
-    echo "destination city: $destCity"
-    echo "destination country: $destCountry"
+    newLine="$Timestamp,$SourceIP,$DestinationIP,$SourcePort,$DestinationPort,$Protocol,$PacketLength,$PacketType,$TrafficType,$PayloadData,$MalwareIndicators,$AnomalyScores,$Alerts,$AttackType,$AttackSignature,$ActionTaken,$SeverityLevel,$UserInformation,$DeviceInformation,$NetworkSegment,$GeoLocation,$ProxyInformation,$FirewallLogs,$IDSIPSAlerts,$LogSource,$destCity,$destCountry"
 
+    processedLineNumber=$((processedLineNumber+1))
+    echo "Number of processed lines: $processedLineNumber"
+    echo $newLine >> $destinationFileName
 done < "$filename"
