fc3f5cdaca7e414ba10684658625acaa8b3a289d
Mateusz Szmyd <Mateusz.Szmyd@grupawp.pl>
2024-04-12 22:54:10 +0200

    move to app.r

--- app.R
+++ app.R
@@ -1,23 +1,20 @@
-# if (!require(shiny)) install.packages("shiny", repos = "http://cran.us.r-project.org")
-# if (!require(shinydashboard)) install.packages("shinydashboard", repos = "http://cran.us.r-project.org")
-# if (!require(leaflet)) install.packages("leaflet", repos = "http://cran.us.r-project.org")
-# if (!require(geojsonio)) install.packages("geojsonio", repos = "http://cran.us.r-project.org")
-# if (!require(RColorBrewer)) install.packages("RColorBrewer", repos = "http://cran.us.r-project.org")
 library(shiny)
 library(shinydashboard)
 library(leaflet)
 library(geojsonio)
 library(RColorBrewer)
+library(ggplot2)
+library(dplyr)
 
 source("map.R")
 
+# Definicja UI
 ui <- navbarPage(
   title = "Cybersecurity Attacks",
   tabPanel("World Map",
     sidebarLayout(
       sidebarPanel(
         selectInput("datatype", "Data Type", choices = c("Target Count", "Source Count", "Anomaly Score", "Payload length")),
-        checkboxInput("circles", "Show Circles", TRUE),
         checkboxInput("legend", "Show legend", TRUE)
       ),
       mainPanel(
@@ -31,9 +28,37 @@ ui <- navbarPage(
       )
     ),
     icon = icon("globe"),
+  ),
+  tabPanel(
+    "Charts",
+    fluidRow(
+      box(
+        title = "Attack Type",
+        status = "primary",
+        plotOutput("attack_type_pie")
+      ),
+      box(
+        title = "Action Taken",
+        status = "primary",
+        plotOutput("action_taken_pie")
+      ),
+      box(
+        title = "Attack Distribution Over Time",
+        status = "primary",
+        plotOutput("attack_time_line")
+      ),
+      box(
+        title = "Countries with Same Source and Destination",
+        status = "primary",
+        plotOutput("same_source_dest_pie")
+      )
+    )
   )
 )
+
+# Definicja server
 server <- function(input, output) {
+  # Wczytanie danych do mapy
   chosenData <- reactive({
     if (input$datatype == "Target Count") {
       world_spdf$Destination_count
@@ -46,10 +71,13 @@ server <- function(input, output) {
     }
   })
 
+  # Funkcja generująca paletę kolorów
   colorpal <- reactive({
     bins <- c(0, 10, 20, 50, 100, 200, 500, 1000, Inf)
     colorBin("Blues", domain = chosenData(), bins = bins)
   })
+
+  # Tekst do wyświetlenia na mapie
   maptext <- reactive({
     paste(
       "Country: ", world_spdf$name_pl, "<br/>",
@@ -59,10 +87,13 @@ server <- function(input, output) {
     ) %>%
       lapply(htmltools::HTML)
   })
+
+  # Renderowanie mapy
   output$map <- renderLeaflet({
     world_map
   })
 
+  # Aktualizacja mapy
   observe({
     pal <- colorpal()
     leafletProxy("map", data = world_spdf) %>%
@@ -83,6 +114,7 @@ server <- function(input, output) {
       )
   })
 
+  # Dodawanie legendy
   observe({
     proxy <- leafletProxy("map", data = world_spdf)
     proxy %>% clearControls()
@@ -97,29 +129,39 @@ server <- function(input, output) {
       )
     }
   })
-  observeEvent(input$map_shape_click, {
-    click <- input$map_shape_click
-    if (is.null(click))
-      return()
-    print(click)
-  
+
+  # Wykres z typem ataków
+  output$attack_type_pie <- renderPlot({
+    pie(table(data$Attack.Type))
   })
-  # observe({
-  #   proxy <- leafletProxy("map", data = world_spdf)
-  #   proxy %>% clearMarkers()
-  #   if (input$circles) {
-  #     pal <- colorpal()
-  #     proxy %>% addCircles(
-  #       lng = ~world_spdf@polygons[[1]]@labpt[1],
-  #       lat = ~world_spdf@polygons[[1]]@labpt[2],
-  #       radius = chosenData() / 100,
-  #       fillColor = ~ pal(chosenData()),
-  #       weight = 1,
-  #       stroke = FALSE,
-  #       fillOpacity = 0.6
-  #     )
-  #   }
-  # })
+
+  # Wykres z typem podjętej akcji
+  output$action_taken_pie <- renderPlot({
+    pie(table(data$Action.Taken))
+  })
+
+  # Wykres liniowy z rozkładem ataków w czasie
+  output$attack_time_line <- renderPlot({
+    data$Timestamp <- as.Date(data$Timestamp)
+    data$Timestamp <- format(data$Timestamp, "%Y-%m")
+    data_attackTime <- data.frame(table(data$Timestamp))
+    data_attackTime$Var1 <- as.Date(paste(data_attackTime$Var1, "-01", sep = ""),
+      format = "%Y-%m-%d"
+    )
+
+    ggplot(data_attackTime, aes(x = Var1, y = Freq)) +
+      geom_point(size = 1) +
+      geom_line() +
+      labs(x = "Czas", y = "Ilość ataków") +
+      theme_minimal()
+  })
+
+  # Procent krajów, w których source = destination
+  output$same_source_dest_pie <- renderPlot({
+    data <- mutate(data, is.same = Destination.Country == Source.Country)
+    pie(table(data$is.same))
+  })
 }
 
+# Uruchomienie aplikacji Shiny
 shinyApp(ui, server)
