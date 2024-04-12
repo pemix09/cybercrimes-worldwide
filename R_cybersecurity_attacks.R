178bed1c7a9907035ed75e3fa91306f4522e8735
Karel223 <69030667+Karel223@users.noreply.github.com>
2024-04-12 19:19:34 +0200

    Update R_cybersecurity_attacks.R
    additiona pie chart

--- R_cybersecurity_attacks.R
+++ R_cybersecurity_attacks.R
@@ -1,14 +1,14 @@
 library(ggplot2)
 library(dplyr)
 
-df <- read.csv("modified_data.csv")
+df <- read.csv("C:/Users/karol/Pulpit/Inf_sem1/wizualizacja/modified_data.csv")
 
 #piechart z typem ataków 
-df_attackType <- df[ ,14]
+df_attackType <- df$Attack.Type
 pie(table(df_attackType))
 
 #piechart z typem podjętej akcji 
-df_actionTaken <- df[ ,16]
+df_actionTaken <- df$Action.Taken
 pie(table(df_actionTaken))
 
 #wykres liniowy z rozkładem ataków w czasie
@@ -21,4 +21,8 @@ df_attackTime$Var1 <- as.Date(paste(df_attackTime$Var1, "-01", sep = ""),
 ggplot(df_attackTime, aes(x = Var1, y = Freq)) + 
   geom_point(size = 1) + geom_line() +
   labs(x = "Czas", y = "Ilość ataków") + theme_minimal()
+ 
 
+#procent krajów w których source = destination 
+df <- mutate(df, is.same = Destination.Country == Source.Country)
+pie(table(df$is.same))
